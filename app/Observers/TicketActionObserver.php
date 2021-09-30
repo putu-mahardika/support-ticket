e675f465c17c1489696c3d8a3983da000e5a8c91
Putu Mahardika <mahardikaputu13@gmail.com>
2021-09-30 17:53:46 +0700

    [=] fix send email

--- app/Observers/TicketActionObserver.php
+++ app/Observers/TicketActionObserver.php
@@ -11,11 +11,19 @@ class TicketActionObserver
 {
     public function created(Ticket $model)
     {
+        // dd($model);
         $data  = ['action' => 'New ticket has been created!', 'model_name' => 'Ticket', 'ticket' => $model];
-        $users = \App\User::whereHas('roles', function ($q) {
+        $users_admin = \App\User::whereHas('roles', function ($q) {
             return $q->where('title', 'Admin');
         })->get();
-        Notification::send($users, new DataChangeEmailNotification($data));
+
+        $users_agent = \App\User::whereHas('roles', function ($q) {
+            return $q->Where('title', 'Agent');
+        })->where('id',$model->assigned_to_user_id)->get();
+        // dd($users);
+        
+        Notification::send($users_admin, new DataChangeEmailNotification($data));
+        Notification::send($users_agent, new DataChangeEmailNotification($data));
     }
 
     public function updated(Ticket $model)
