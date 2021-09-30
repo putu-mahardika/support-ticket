6095641c9b3321f25677be4af432ae1ddc3defba
Putu Mahardika <mahardikaputu13@gmail.com>
2021-09-30 17:54:23 +0700

    [=] update kolom tiket nullable

---
+++ database/migrations/2021_09_30_070612_update_status_category_priority_default_ticket.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateStatusCategoryPriorityDefaultTicket extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->unsignedInteger('status_id')->nullable()->change();
+            $table->unsignedInteger('priority_id')->nullable()->change();
+            $table->unsignedInteger('category_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $table->unsignedInteger('status_id')->change();
+        $table->unsignedInteger('priority_id')->change();
+        $table->unsignedInteger('category_id')->change();
+    }
+}
--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,10 +3,6 @@
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
 {{-- <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo"> --}}
-<<<<<<< HEAD
-=======
-<img src="{{ asset('/images/logo-group-1.png') }}" class="logo" alt="Laravel Logo">
->>>>>>> f4f167cd8a10c0963ae35d53ec5ff0c8799d012c
 @else
 {{ $slot }}
 @endif
