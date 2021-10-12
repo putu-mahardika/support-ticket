a67920644547648760c6e09557b829a8dabcc1d0
ekkys <ekkys99@gmail.com>
2021-10-12 11:29:39 +0700

    Persiapan membuat notifikasi

--- resources/views/admin/notif/notifpage.blade.php
+++ resources/views/admin/notif/notifpage.blade.php
@@ -1,4 +1,8 @@
 @extends('layouts.admin')
 @section('content')
-<h1>Ini Notifications</h1>
+<h3>Notifikasi</h3>
+<div class="list-group">
+    <a href="id_tiket" class="list-group-item list-group-item-action"><img style="width: 40px; height:auto" src="{{ asset('theme/img/headset.png') }}"><span class="">         Ticket baru #deskripsi tiket dari database</span></a>
+    <a href="id_tiket" class="list-group-item list-group-item-action"><img style="width: 40px; height:auto" src="{{ asset('theme/img/headset.png') }}"><span class="">         Ticket baru #deskripsi tiket dari database</span></a>
+</div>
 @endsection
