3799cf6457ec5beae35a9763db6872621911b109
Agung Setyo Pribadi <agungsetyopribadi8@gmail.com>
2021-10-19 11:08:37 +0700

    Perbaikan view login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,29 +7,35 @@
                 <!-- Nested Row within Card Body -->
                 <div class="row">
                     <div class="col-lg-6">
-                        <div class="p-5">
-                            <div class="container mb-3">
-                                <img src="{{ asset('theme/img/logo-group-1.png') }}" width="100%" height="auto">
-                                <h3 class="text-center mb-2">Help Desk</h3>
+                        <div class="pt-5 pl-5 pr-5 pb-0">
+                            <div class="text-center mb-5">
+                                <img src="{{ asset('theme/img/logo-group-1.png') }}" style="width: 15rem; height: auto;">
+                                <h3 class="text-center mb-2">
+                                    {{ config('app.name') }}
+                                </h3>
                                 {{-- <p class="text-muted">{{ trans('global.login') }}</p> --}}
                             </div>
 
                             @if(session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
                             @endif
 
                             <form class="user" method="POST" action="{{ route('login') }}">
-                            @csrf
+                                @csrf
                                 <div class="form-group">
                                     <input class="form-control form-control-user"
-                                    id="email" name="email" type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" required autocomplete="email" autofocus placeholder="{{ trans('global.login_email') }}" value="{{ old('email', null) }}">
+                                           id="email" name="email"
+                                           type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                                           required autocomplete="email"
+                                           autofocus placeholder="{{ trans('global.login_email') }}"
+                                           value="{{ old('email', null) }}">
 
                                     @if($errors->has('email'))
-                                    <div class="invalid-feedback">
-                                        {{ $errors->first('email') }}
-                                    </div>
+                                        <div class="invalid-feedback">
+                                            {{ $errors->first('email') }}
+                                        </div>
                                     @endif
                                 </div>
 
@@ -38,50 +44,38 @@
                                     <div class="input-group">
                                         <input type="password" class="form-control form-control-user" style="border-radius:10rem;" id="password" name="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" required placeholder="{{ trans('global.login_password') }}">
                                         <span class="input-group-text" onclick="password_show_hide();" style="cursor: pointer; background-color:transparent;border:none;margin-left:-45px;z-index:10;">
-                                        <i class="fas fa-eye-slash" id="btn-eye"></i>
-                                        {{-- <i class="fas fa-eye-slash d-none" id="hide_eye"></i> --}}
+                                            <i class="fas fa-eye-slash" id="btn-eye"></i>
                                         </span>
                                     </div>
-                                    
                                     @if($errors->has('password'))
-                                    <div class="invalid-feedback">
-                                        {{ $errors->first('password') }}
-                                    </div>
+                                        <div class="invalid-feedback">
+                                            {{ $errors->first('password') }}
+                                        </div>
                                     @endif
-                                    
                                 </div>
 
-                                <button type="submit" class="btn btn-primary btn-user btn-block"">
-                                    {{ trans('global.login') }}
+                                <button type="submit" class="btn btn-primary btn-user btn-block">
+                                    <i class="fas fa-sign-in-alt"></i> {{ trans('global.login') }}
                                 </button>
                             </form>
                             <hr>
                             <div class="text-center">
                                 @if(Route::has('password.request'))
-                                <a class="small" href="{{ route('password.request') }}">
-                                    {{ trans('global.forgot_password') }}
-                                </a><br>
+                                    <a class="small" href="{{ route('password.request') }}">
+                                        {{ trans('global.forgot_password') }}
+                                    </a><br>
                                 @endif
                             </div>
-                        </div>
-                        <div class="d-flex align-items-end pl-5">
-                            <br><br><br><br><br>
-                            <small>V.1.(081021)</small>
+                            <p class="small mt-5 text-center">@version('compact')</p>
                         </div>
                     </div>
-                    <div class="col-lg-6">
-                        <img src="{{ asset('theme/img/bg-login.jpg') }}" style="width: auto; height:  600px; position: left;" >
+                    <div class="col-lg-6 d-none d-lg-block">
+                        <img src="{{ asset('theme/img/bg-login.jpg') }}" style="width: auto; height: 32.5rem; position: left;" >
                     </div>
-                </div>
-                <div class="row">
-
                 </div>
             </div>
-
         </div>
-
     </div>
-
 </div>
 @endsection
 
