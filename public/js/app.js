db318d9a768b96da4d994b9aff886a1d87e74f1e
ekkys <ekkys99@gmail.com>
2021-10-19 11:20:43 +0700

    Update version v1.1.0

--- public/js/app.js
+++ public/js/app.js
@@ -19012,8 +19012,8 @@ function isValidJson(string) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/monstercode/projects/support-ticket/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/monstercode/projects/support-ticket/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\support-ticket\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\support-ticket\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ }),
