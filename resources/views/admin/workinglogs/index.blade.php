0c495b4a8f3727af05ef496db9a108a6c959edfc
Agung Setyo Pribadi <agungsetyopribadi8@gmail.com>
2021-10-27 09:52:50 +0700

    Change datatables to devExtreme

--- resources/views/admin/workinglogs/index.blade.php
+++ resources/views/admin/workinglogs/index.blade.php
@@ -1,97 +1,47 @@
 @extends('layouts.admin')
 @section('content')
-@can('project_create')
-    <div style="margin-bottom: 10px;" class="row">
-        <div class="col-lg-12">
-            <a class="btn btn-success" href="{{ route("admin.projects.create") }}">
-                {{ trans('global.add') }} {{ trans('cruds.project.title_singular') }}
-            </a>
-        </div>
-    </div>
-@endcan
-<div class="card">
-    <div class="card-header">
-        Working Logs
-    </div>
-
-    <div class="card-body">
-        <div class="table-responsive">
-            <table class=" table table-bordered table-striped table-hover datatable datatable-Project">
-                <thead>
-                    <tr>
-                        <th width="10">
-
-                        </th>
-                        <th>
-                            No
-                        </th>
-                        <th>
-                            Kode
-                        </th>
-                        <th>
-                            Status
-                        </th>
-                        <th>
-                            Stared At
-                        </th>
-                        <th>
-                            Finished At
-                        </th>
-                    </tr>
-                </thead>
-
-            </table>
+    <div class="card">
+        <div class="card-header">
+            Working Logs
         </div>
 
-
+        <div class="card-body">
+            <div id="gridContainer"></div>
+        </div>
     </div>
-</div>
 @endsection
 @section('scripts')
-@parent
-<script>
-    $(function () {
-  let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
-@can('project_delete')
-  let deleteButtonTrans = '{{ trans('global.datatables.delete') }}';
-  let deleteButton = {
-    text: deleteButtonTrans,
-    url: "{{ route('admin.projects.massDestroy') }}",
-    className: 'btn-danger',
-    action: function (e, dt, node, config) {
-      var ids = $.map(dt.rows({ selected: true }).nodes(), function (entry) {
-          return $(entry).data('entry-id')
-      });
-
-      if (ids.length === 0) {
-        alert('{{ trans('global.datatables.zero_selected') }}')
-
-        return
-      }
-
-      if (confirm('{{ trans('global.areYouSure') }}')) {
-        $.ajax({
-          headers: {'x-csrf-token': _token},
-          method: 'POST',
-          url: config.url,
-          data: { ids: ids, _method: 'DELETE' }})
-          .done(function () { location.reload() })
-      }
-    }
-  }
-  dtButtons.push(deleteButton)
-@endcan
-
-  $.extend(true, $.fn.dataTable.defaults, {
-    order: [[ 1, 'desc' ]],
-    pageLength: 100,
-  });
-  $('.datatable-Project:not(.ajaxTable)').DataTable({ buttons: dtButtons })
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function(e){
-        $($.fn.dataTable.tables(true)).DataTable()
-            .columns.adjust();
-    });
-})
-
-</script>
+    @parent
+    <script src="https://cdn3.devexpress.com/jslib/21.1.5/js/dx.all.js"></script>
+    <script>
+        $(function () {
+            $(document).ready(() => {
+                getData();
+            });
+            function getData() {
+                $("#gridContainer").dxDataGrid({
+                    dataSource: "{{ route('admin.workinglogs.data') }}",
+                    keyExpr: 'id',
+                    sorting: {
+                        mode: "multiple"
+                    },
+                    columns: [
+                        {
+                            caption: '#',
+                            cellTemplate: function(cellElement, cellInfo) {
+                                cellElement.text(cellInfo.row.rowIndex + 1);
+                            }
+                        },
+                        {dataField: "ticket.title", caption: "Ticket"},
+                        {dataField: "status.name", caption: "Status"},
+                        {dataField: "started_at", caption: "Start", dataType: "datetime", format: "yyyy-MM-dd HH:mm:ss"},
+                        {dataField: "finished_at", caption: "End", dataType: "datetime", format: "yyyy-MM-dd HH:mm:ss"}
+                    ],
+                    showBorders: true,
+                    filterRow: { visible: true },
+                    hoverStateEnabled: true
+                });
+            };
+        });
+    </script>
 @endsection
